465f7b431c2890d6205d897a386f5779b463e308
Alceu-wv <alceu.wv@gmail.com>
2022-10-27 14:36:50 -0300

    test: pagination_generator

--- tests/test_pagination_generator.py
+++ tests/test_pagination_generator.py
@@ -1,5 +1,5 @@
 import pytest
-
+from typing import Iterable
 from paginator_generator.generator import PaginationGenerator
 
 @pytest.mark.parametrize(
@@ -57,11 +57,45 @@ def test__get_end(paginator: PaginationGenerator, pages, boundaries, result_to_a
     assert result == result_to_assert
     assert len(result) == paginator.boundaries
 
-def test__clean_numbers_out_of_range():
-    pass
 
-def test__get_indexes_to_be_fold():
-    pass
+@pytest.mark.parametrize(
+    "total_pages, list_to_clean, result_to_assert",
+    (
+        (10, [-2,-1, 0, 1], [1]),
+        (10, [9, 10, 11], [9, 10]),
+    ),
+    ids=[
+        "under_zero",
+        "over_total_pages"
+    ],
+)
+def test__clean_numbers_out_of_range(paginator: PaginationGenerator, total_pages, list_to_clean, result_to_assert):
+    paginator.total_pages = total_pages
+    
+    result = paginator._clean_numbers_out_of_range(list_to_clean)
+    
+    assert result == result_to_assert
+    assert isinstance(result, list)
+
+@pytest.mark.parametrize(
+    "list_to_parse, indexes_to_be_fold",
+    (
+        ([0, 2], [1]),
+        ([10, 20], [1]),
+        ([10, 12, 13, 15], [3, 1]),
+        ([10, 20, 30], [2, 1]),
+    ),
+    ids=[
+        "one_interval_short",
+        "one_interval_long",
+        "two_interval_short",
+        "two_interval_long",
+    ],
+)
+def test__get_indexes_to_be_fold(paginator: PaginationGenerator, list_to_parse, indexes_to_be_fold):
+    result = paginator._get_indexes_to_be_fold(list_to_parse)
+    
+    assert list(result) == indexes_to_be_fold
 
 def test_build_pagination():
     pass
