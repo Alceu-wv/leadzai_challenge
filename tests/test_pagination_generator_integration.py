37154e1e21b5d2c1023a9678dc03079ed7e0bc21
Alceu-wv <alceu.wv@gmail.com>
2022-10-31 09:10:11 -0300

    test: PaginationGenerator test structure

---
+++ tests/test_pagination_generator_integration.py
@@ -0,0 +1,34 @@
+def test_pagination_start_at_one_with_boundaries():
+    pass
+
+
+def test_big_interval():
+    pass
+
+
+def test_short_interval():
+    pass
+
+
+def test_around_or_boundaries_greater_than_total_pages():
+    pass
+
+
+def test_negative_inputs():
+    pass
+
+
+def test_current_page_zero():
+    pass
+
+
+def test_total_pages_zero():
+    pass
+
+
+def test_boundaries_zero():
+    pass
+
+
+def test_aroud_zero():
+    pass
