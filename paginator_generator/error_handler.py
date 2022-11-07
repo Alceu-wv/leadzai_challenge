3430425c5ba7270220cded83cefc3d1fb9d92caa
Alceu-wv <alceu.wv@gmail.com>
2022-11-07 16:06:31 -0300

    refact: error_handler

--- paginator_generator/error_handler.py
+++ paginator_generator/error_handler.py
@@ -16,12 +16,6 @@ class PaginationGeneratorErrorHandler:
         self.boundaries = boundaries
         self.around = around
 
-    def _check_short_total_pages(self):
-        if self.total_pages < self.boundaries + self.around:
-            raise PaginationGeneratorError(
-                "'boundaries' and 'around' sum cannot be greater than total pages."
-            )
-
     def _check_current_page_out_of_range(self):
         if self.current_page not in list(range(self.total_pages + 1)):
             raise PaginationGeneratorError(
@@ -29,5 +23,4 @@ class PaginationGeneratorErrorHandler:
             )
 
     def check_for_input_errors(self):
-        self._check_short_total_pages()
         self._check_current_page_out_of_range()
